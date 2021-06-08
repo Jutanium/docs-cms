779f572b45367da2e70f31d69a2468f4c612815a
Dan Jutan <danjutan@gmail.com>
2021-06-08 09:38:32 -0400

    add root tsconfig to make IDE smarter

---
+++ packages/google-docs-components/dist/cjs/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.componentsFromDoc = void 0;
+function componentsFromDoc(doc) {
+    console.log(doc.body);
+    console.log(doc.potato);
+}
+exports.componentsFromDoc = componentsFromDoc;
+//# sourceMappingURL=index.js.map
---
+++ packages/google-docs-components/src/index.ts
@@ -0,0 +1,5 @@
+import type { document, elementTypes } from "parse-google-docs"
+
+export function componentsFromDoc(doc: document) {
+  console.log(doc.body);
+}
