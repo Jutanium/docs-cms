a47d98ae30c0111f20932f17eee19100160de50e
Dan Jutan <danjutan@gmail.com>
2021-06-09 16:30:01 -0400

    Revert "watch commands"
    This reverts commit e625d557de3494e4bdf249d23fe23a96b929a8c6.

---
+++ packages/google-docs-components/dist/cjs/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.componentsFromDoc = void 0;
+function componentsFromDoc(doc) {
+    console.log(doc.body);
+}
+exports.componentsFromDoc = componentsFromDoc;
+//# sourceMappingURL=index.js.map
