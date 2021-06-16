f62431e0df63b19d20ad81c3a59bcaebb4589858
Dan Jutan <danjutan@gmail.com>
2021-06-16 16:20:10 -0400

    vue2 library setup

--- packages/vue2/src/main.ts
+++ packages/vue2/src/main.ts
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './TestApp.vue'
-import Renderer from "@/components/Renderer";
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-export default Renderer;
