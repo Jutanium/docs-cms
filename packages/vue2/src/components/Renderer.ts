9d3891b69dc87616ad7edfe8d5abcf04e7e13830
Dan Jutan <danjutan@gmail.com>
2021-06-17 13:44:38 -0400

    inline slot support

--- packages/vue2/src/components/Renderer.ts
+++ packages/vue2/src/components/Renderer.ts
@@ -14,13 +14,50 @@ export default Vue.extend({
     components: {
       type: Object,
       default: () => ({})
+    },
+    inlineSlotFormat: {
+      type: RegExp,
+      default: () => (
+        /\[\^(\d*[a-zA-z]+\d*)\](?:(.+)\[\/\1\])*/g
+      )
     }
   },
   render (h, context) {
-    function fromContent (data: ContentData): VNode | VNode[] | string | false {
+    function fromContent (data: ContentData): VNode | Array<String | VNode> | string | false {
       if (typeof data == "string") {
-        return data;
+        const inlineSlotMatches = data.matchAll(context.props.inlineSlotFormat);
+
+        const insertions = [];
+        for (const match of inlineSlotMatches) {
+          if (match.index == undefined) continue;
+          const slotName = match[1];
+          const inner = match[2];
+          if (slotName in context.scopedSlots) {
+            const resolvedSlot = context.scopedSlots[slotName]({inner});
+            if (resolvedSlot) {
+              const start = match.index;
+              const end = start + match[0].length;
+              insertions.push({start, end, slot: resolvedSlot})
+            }
+          }
+        }
+
+        if (!insertions.length) return data;
+        const returnArr: Array<string | VNode> = [];
+        let lastIndex = 0;
+        insertions
+          .sort((a, b) => a.start - b.start)
+          .forEach(({start, end, slot}) => {
+            if (lastIndex != start)
+              returnArr.push(data.slice(lastIndex, start))
+            returnArr.push(...slot);
+            lastIndex = end;
+          })
+        if (lastIndex != data.length)
+          returnArr.push(data.slice(lastIndex));
+        return returnArr;
       }
+
       if ("element" in data) {
         const nodeData: VNodeData = {
           ...(data.style && {style: data.style}),
@@ -51,7 +88,7 @@ export default Vue.extend({
       return false;
     }
     function fromContentArray (contentArray: ProcessedContent) {
-      return contentArray.map(fromContent).filter(Boolean);
+      return contentArray.map(data => fromContent(data)).flat(1).filter(Boolean) as Array<VNode>;
     }
 
     return h("div", fromContentArray(context.props.content));
