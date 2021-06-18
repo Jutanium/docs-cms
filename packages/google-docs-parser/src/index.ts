3b81c9b957a0a31c6c07b358a39483c26f676c68
Dan Jutan <danjutan@gmail.com>
2021-06-18 17:29:55 -0400

    export getFilesInFolder

--- packages/google-docs-parser/src/index.ts
+++ packages/google-docs-parser/src/index.ts
@@ -1,5 +1,5 @@
-import { getDoc } from "./connect"
+import { getDoc, getFilesInFolder} from "./connect"
 import { parseDoc, document} from "./parse";
 import type * as elementTypes from "./element-matchers/default"
 import type { element} from "./element-matchers/types";
-export { getDoc, parseDoc, document, element, elementTypes }
+export { getDoc, parseDoc, getFilesInFolder, document, element, elementTypes }
